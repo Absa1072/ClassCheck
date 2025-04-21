092c3a2d1510071e5c33723f077410d59980d25c
psp129 <psp129@scarletmail.rutgers.edu>
2025-04-21 17:08:44 -0400

    Update geolocation.js

--- docs/geolocation.js
+++ docs/geolocation.js
@@ -15,7 +15,7 @@ let map, infoWindow;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.5038, lng: -74.4628 },
+    center: { lat: -40.5038, lng: 74.4628 },
     zoom: 12,
   });
   infoWindow = new google.maps.InfoWindow();
