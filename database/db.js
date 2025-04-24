64f1b40fae500035f347ec1c89030f6c8ae6c088
rashmee-g <rashmeegade@gmail.com>
2025-04-24 00:06:56 -0400

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const db = require('mysql2');
 const connection = db.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'M@ster0f@ll', 
+  password: '', 
   database: 'classcheck'
 });
 
