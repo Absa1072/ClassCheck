9bae787fa3739ba9209fe92604c6a3db4d0d7b33
psp129 <psp129@scarletmail.rutgers.edu>
2025-04-28 13:49:49 -0400

    Update firebase.js
    put in the new database

--- docs/firebase.js
+++ docs/firebase.js
@@ -1,20 +1,22 @@
-// Import the database from Firebase using the API key
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAAbcJ3zCGmz4fm9Bpq7Pr0nU6Pr9m8VYY",
-  authDomain: "classcheck-44237.firebaseapp.com",
-  databaseURL: "https://classcheck-44237-default-rtdb.firebaseio.com",
-  projectId: "classcheck-44237",
-  storageBucket: "classcheck-44237.firebasestorage.app",
-  messagingSenderId: "223098378880",
-  appId: "1:223098378880:web:79a616def53d5c3b50a465",
-  measurementId: "G-5971Z3SLTV"
+  apiKey: "AIzaSyC59MPssGSBhB4R4jDYjWv-D-hG1gl0hEA",
+  authDomain: "causal-cacao-457203-s1.firebaseapp.com",
+  databaseURL: "https://causal-cacao-457203-s1-default-rtdb.firebaseio.com",
+  projectId: "causal-cacao-457203-s1",
+  storageBucket: "causal-cacao-457203-s1.firebasestorage.app",
+  messagingSenderId: "604345231228",
+  appId: "1:604345231228:web:c61b68eccff94d1d5826f9",
+  measurementId: "G-MJMBCWMNKP"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-// export the firebaseConfig so it can be used in other html files when trying to access the database
-export { firebaseConfig };
+export {firebaseConfig};
