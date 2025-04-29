869d3c281bb27975580d1b650474592f6f8ccde0
psp129 <psp129@scarletmail.rutgers.edu>
2025-04-29 14:53:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-const { auth, requiresAuth } = require('express-openid-connect');
+const { auth } = require('express-openid-connect');
 
 const app = express();
 
-app.use('/docs', express.static(path.join(__dirname, 'docs')));
+app.use('/docs',express.static(path.join(__dirname, 'docs')));
 
-// Auth0 config
 const config = {
   authRequired: false,
   auth0Logout: true,
@@ -16,27 +15,20 @@ const config = {
   issuerBaseURL: 'https://dev-csb64xqu8rysh5zp.us.auth0.com'
 };
 
-// middleware
+// auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
+//middleware
+app.use(auth(config));
 
-// get netID
-app.get('/current-user', requiresAuth(), (req, res) => {
-  const email = req.oidc.user?.email || '';
-  const netID = email.split('@')[0];
-  res.json({ netID });
-});
-
-app.get('/myClasses', requiresAuth(), (req, res) => {
-  res.sendFile(path.join(__dirname, 'docs/myClasses.html'));
-});
+const { requiresAuth } = require('express-openid-connect');
 
 app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user, null, 2));
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 app.listen(3000, () => {
