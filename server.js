a4a5898ce569318778d6a66e342de4897331ad0a
psp129 <psp129@scarletmail.rutgers.edu>
2025-05-02 21:15:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'docs')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const config = {
   authRequired: false,
