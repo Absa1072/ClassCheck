c803ea1d183bb02b714ba857bd749c501446635d
navyaterap <nterap22@gmail.com>
2025-05-05 17:21:22 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,6 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'ClassCheckMail@gmail.com',
-        pass: 'qpncibsiedepdwke'
-    }
-});
 
 app.use(express.json());
 app.use('/', routes);
